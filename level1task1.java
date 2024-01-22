38fc18cad52e2586d788b11e07ea866efe9984cb
Abhishekshirke6998 <133661290+Abhishekshirke6998@users.noreply.github.com>
2024-01-23 01:10:32 +0530

    Add files via upload

---
+++ level1task1.java
@@ -0,0 +1,13 @@
+public class level1task1 {
+
+
+        public static void main(String args[]) {
+         
+                 float Fahrenheit, Celsius;
+                 Celsius = 13;
+                 Fahrenheit = ((Celsius * 9) / 5) + 32;
+                 System.out.println("Temperature in Fahrenheit is: " + Fahrenheit);
+             }
+         }
+     
+
